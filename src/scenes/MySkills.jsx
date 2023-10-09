777c32df04b3005412683b69cddb0c8a171902bd
rehanhussa <Technosis141@gmail.com>
2023-10-08 22:38:52 -0400

    projects

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,132 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+          "Empowered by a diverse skill set, I blend technical expertise with creativity, innovation, and imagination to craft solutions that resonate and make an impact.
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIENCE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Experience
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          A Software Engineer with a comprehensive skill set spanning multiple programming languages, databases, and frameworks. A graduate of General Assembly's intensive Software Engineering Immersive program, I have showcased my prowess in developing user-centric applications, such as the visually engaging 'Simple Hangman' and the music application 'Betterify'. Beyond his technical acumen, my experience also extends to managing Minecraft server communities, where I have led teams to ensure optimal gameplay. In the realm of traditional employment, I have demonstrated exemplary leadership as a Retail Sales Manager at GUARI INC and as a Restaurant Manager at Crown Fried Chicken. 
+          </p>
+        </motion.div>
+
+        {/* INNOVATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Innovative
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          Innovation is at the core of everything I do. From developing immersive music applications to pioneering customer-centric initiatives in the restaurant sector, I consistently push boundaries to deliver fresh, impactful solutions. My diverse background, spanning software engineering to team leadership, equips me with a unique perspective that I leverage to approach challenges differently. Whether I'm optimizing code, designing user interfaces, or implementing new strategies in traditional sectors, my commitment to innovation drives me to explore uncharted territories and create transformative experiences. Dive into my portfolio to witness how I've turned groundbreaking ideas into tangible results.
+          </p>
+        </motion.div>
+        {/* IMAGINATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Imaginative
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          "Imagination is the catalyst behind my every endeavor. My journey in software engineering and leadership roles is a testament to my ability to envision what's beyond the horizon and bring those visions to life. From conceptualizing engaging web applications to reimagining customer experiences in diverse sectors, my imaginative spirit fuels my drive to create and innovate. My projects, whether technical or managerial, are imbued with a touch of creativity that sets them apart. Explore my portfolio to see how my imaginative flair has transformed challenges into unique and impactful solutions.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
